f980da4f4b566479f8b07b0b95489b19122dc931
Bryan Ducourt <2190400201@stu.hit.edu.cn>
2021-11-17 17:15:01 +0800

    add GenerateFirstSet, updated some functions

--- ex2/main.cpp
+++ ex2/main.cpp
@@ -5,12 +5,16 @@
 #include "vector"
 #include "map"
 #include "string"
+#include "list"
+#include "algorithm"
 
 using namespace std;
-
+map<string, vector<string >> FirstSet;
+map<string, vector<string >> FollowSet;
 map<string, int> NonTerminalMap;
 map<string, int> TerminalMap;
 int closure_cnt = 0;
+int cnt = 0;
 bool IsNonTerminal(const string &string1) {
     return (NonTerminalMap.find(string1) != NonTerminalMap.end());
 }
@@ -55,7 +59,9 @@ public:
     int position_of_dot;
     Production p;
     ulong len;
-
+    bool operator==(const Item& t) const{
+        return (p.P==t.p.P && position_of_dot==t.position_of_dot);
+    }
     Item(int pod, Production production) {
         position_of_dot = pod;
         p = std::move(production);
@@ -76,21 +82,29 @@ vector<Item> Create_Item_List(vector<Production> p_list) {
 
 class Closure {
 public:
-    int num_of_closure{};
+//    int num_of_closure{};
     vector<Item> items;
+    bool operator==(const Closure &tgt) const{
+        return (items==tgt.items);
+    }
 };
 
-Closure Compute_Closure_I(int num_of_closure, vector<Item> target_list, vector<Item> &global_list) {
+Closure Compute_Closure_I(int num_of_closure, list<Item> target_list, vector<Item> &global_list) {
     Closure c;
     vector<Item> t;
-    c.num_of_closure = num_of_closure;
+//    c.num_of_closure = num_of_closure;
     for (auto it = target_list.begin(); it != target_list.end(); it++) {
         t.push_back(*it);
         if (it->position_of_dot != it->len) {
             if (IsNonTerminal(it->p.P[it->position_of_dot])) {
                 for (auto it_ = global_list.begin(); it_ != global_list.end(); it_++) {
                     if (it_->position_of_dot == 0 && it_->p.V == it->p.P[it->position_of_dot]) {
-                        t.push_back(*it_);
+                        if (IsNonTerminal(it_->p.P[it_->position_of_dot])){
+                            if (find(target_list.begin(),target_list.end(),*it_)==target_list.end())
+                            target_list.push_back(*it_);
+                        } else
+                        if (find(t.begin(),t.end(),*it_)==t.end())
+                            t.push_back(*it_);
                     }
                 }
             }
@@ -109,17 +123,67 @@ void GenerateSymbolTable(vector<string>& table){
     }
 }
 
-void Go(Closure i, const string& x, vector<Item>& global, vector<Closure>& closure_set) {
-    vector<Item> t;
+void Go(Closure i, const string& x, vector<Item>& global, list<Closure>& closure_set) {
+    list<Item> t;
     for (auto it = i.items.begin(); it != i.items.end(); it++) {
         if (it->position_of_dot != it->len && it->p.P[it->position_of_dot] == x) {
             Item _t(it->position_of_dot+1, it->p);
             t.push_back(_t);
         }
     }
-    if (!t.empty())
-        closure_set.push_back(Compute_Closure_I(++closure_cnt,t,global));
+    if (!t.empty()) {
+        Closure ii=Compute_Closure_I(++closure_cnt, t, global);
+        auto it = find(closure_set.begin(),closure_set.end(),ii);
+        if (it == closure_set.end())
+            closure_set.push_back(ii);
+    }
+}
 
+void Merge(vector<string>& dest, vector<string>& source){
+    for (auto it = source.begin();it!=source.end();it++){
+        auto it_ = find(dest.begin(),dest.end(),*it);
+        if (it_==dest.end()){
+            dest.push_back(*it);
+        }
+    }
+}
+
+void Merge(vector<string>&dest, string& source){
+    auto it = find(dest.begin(), dest.end(), source);
+    if (it == dest.end()){
+        dest.push_back(source);
+    }
+}
+
+void GenerateFirstSet(map<string,vector<string>>& firstset,const string& target, vector<Production>& plist){
+    vector<Production> t;
+    vector<string > s;
+    for(auto it=plist.begin();it!=plist.end();it++){
+        if (it->V==target){
+            t.push_back(*it);
+        }
+    }
+    for (auto it = t.begin();it!=t.end();it++){
+        if (IsNonTerminal(it->P[0])){
+            if (it->P[0]!=target) {
+                GenerateFirstSet(firstset, it->P[0], plist);
+                Merge(s, firstset[it->P[0]]);
+            }
+            if (it->P.size()>1 && IsNonTerminal(it->P[1])){
+                if (it->P[1]!=target){
+                    GenerateFirstSet(firstset, it->P[1], plist);
+                    Merge(s, firstset[it->P[1]]);
+                }
+            }
+        } else {
+            Merge(s, it->P[0]);
+        }
+    }
+    firstset.insert(pair<string, vector<string>>(target, s));
+}
+
+void GenerateFollowSet(map<string, vector<string>>& followset, string& target){
+    
 }
 
 int main() {
@@ -132,11 +196,23 @@ int main() {
     p = Production_Pre_Process(&file);
     i = Create_Item_List(p);
     GenerateSymbolTable(symbol_table);
-    Closure c;
-    vector<Item> ll = {i[0]};
-    c = Compute_Closure_I(closure_cnt, ll, i);
-    vector<Closure> g;
-    Go(c,"D",i, g);
+    for (auto it = NonTerminalMap.begin();it!=NonTerminalMap.end();it++){
+        GenerateFirstSet(FirstSet,it->first,p);
+    }
+    Closure i_0, i1;
+    list<Item> ll = {i[0]};
+    i_0 = Compute_Closure_I(closure_cnt, ll, i);
+    list<Closure> g;
+    g.push_back(i_0);
+    for (auto it = g.begin();it!=g.end();it++){
+        cout<<cnt++<<endl;
+        for (auto it_ = symbol_table.begin();it_!=symbol_table.end();it_++){
+            Go(*it,*it_,i, g);
+        }
+    }
+    cout<<"end";
+
+
 
     return 0;
 }
